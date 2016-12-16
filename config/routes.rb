babc59a89899e9806d7c1fc69356eb7c0d2f78db
Mbele Lebohang <micheallm95@gmail.com>
2016-12-16 12:20:00 +0200

    finished the toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root 'users#index'
   resources :microposts
   resources :users
-  root 'users#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
